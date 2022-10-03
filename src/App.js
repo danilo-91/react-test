48bb28c6b4cb9da24c03befd7c4d5de1943ff011
Daniel Ise <ise.daniel@gmail.com>
2022-10-03 20:43:46 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src={logo} width="400px" />
+      </nav>
+    </header>
+  );
+}
+
+function Title() {
+  return (
+    <h1>
+      Reasons for studying React
+    </h1>
+  );
+}
+
+function OrderedList() {
+  return (
+    <div>
+      <ul>
+        <li>Some <b>reason</b> to study React</li>
+        <li>Some <b>reason</b> to study React</li>
+        <li>Some <b>reason</b> to study React</li>
+        <li>Some <b>reason</b> to study React</li>
+      </ul>
+    </div>
+  )
+}
+
+function Footer() {
+return (
+  <footer>
+    &copy; 2022 Danilo Development. Todos los rights reserved.
+  </footer>
+)
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Title />
+	<OrderedList />
+	<Footer />
+      </div>
+
+  );
+}
+
+export default App; //wtf default means
